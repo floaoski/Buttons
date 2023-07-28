1f5af1e441afbe3461e91e3de770ea9d114019bc
floaoski <140770451+floaoski@users.noreply.github.com>
2023-07-27 21:41:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,47 @@
-// Get the counter element
+// script.js
 const counterElement = document.getElementById('count');
-
-// Get the add and subtract buttons
 const addBtn = document.getElementById('addBtn');
 const subtractBtn = document.getElementById('subtractBtn');
+const highestAddingUsersElement = document.getElementById('highestAddingUsers');
+const highestSubtractingUsersElement = document.getElementById('highestSubtractingUsers');
 
-// Function to update the counter on the page and store it in localStorage
+let counter = 0;
+
 function updateCounter(value) {
   counter = value;
   counterElement.textContent = counter;
-  localStorage.setItem('counterValue', counter);
 }
 
-// Check if the counter value exists in localStorage, otherwise set it to 0
-let counter = parseInt(localStorage.getItem('counterValue')) || 0;
-updateCounter(counter);
+function updateLeaderboards() {
+  fetch('/api/leaderboards')
+    .then(response => response.json())
+    .then(data => {
+      highestAddingUsersElement.innerHTML = data.highestAddingUsers.map(user => `<li>${user.username}: ${user.totalAdditions}</li>`).join('');
+      highestSubtractingUsersElement.innerHTML = data.highestSubtractingUsers.map(user => `<li>${user.username}: ${user.totalSubtractions}</li>`).join('');
+    })
+    .catch(error => console.error('Error fetching leaderboards:', error));
+}
 
-// Event listeners for buttons
 addBtn.addEventListener('click', () => {
-  counter += 1;
-  updateCounter(counter);
+  fetch('/api/add', { method: 'POST' })
+    .then(response => response.json())
+    .then(data => {
+      updateCounter(data.counterValue);
+      updateLeaderboards();
+    })
+    .catch(error => console.error('Error adding 1:', error));
 });
 
 subtractBtn.addEventListener('click', () => {
-  counter -= 1;
-  updateCounter(counter);
+  fetch('/api/subtract', { method: 'POST' })
+    .then(response => response.json())
+    .then(data => {
+      updateCounter(data.counterValue);
+      updateLeaderboards();
+    })
+    .catch(error => console.error('Error subtracting 1:', error));
 });
+
+// Initial counter update and leaderboard fetch
+updateCounter(counter);
+updateLeaderboards();
